381daf70d9f5458170c32287edb3076ab26babf6
juzik <sergejburov468@gmail.com>
2026-06-07 07:37:15 +0300

    chore: update GTA5Optimizer\src\GTA5Optimizer.UI\Views\MainWindow.xaml.cs

--- GTA5Optimizer/src/GTA5Optimizer.UI/Views/MainWindow.xaml.cs
+++ GTA5Optimizer/src/GTA5Optimizer.UI/Views/MainWindow.xaml.cs
@@ -2,14 +2,21 @@ using GTA5Optimizer.UI.ViewModels;
 
 namespace GTA5Optimizer.UI.Views;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow(MainWindowViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        DragMove();
     }
 }
