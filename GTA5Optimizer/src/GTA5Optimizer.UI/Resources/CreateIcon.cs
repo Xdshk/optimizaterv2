50d229d87310d6f91a6c003cfb2cb3386e8338ae
juzik <sergejburov468@gmail.com>
2026-06-07 15:41:08 +0300

    chore: add GTA5Optimizer\src\GTA5Optimizer.UI\Resources\CreateIcon.cs

---
+++ GTA5Optimizer/src/GTA5Optimizer.UI/Resources/CreateIcon.cs
@@ -0,0 +1,3 @@
+// This file is a placeholder. The app.ico should be placed in this folder.
+// You can create it from any .png using an online converter.
+// For now, the build will use the default icon.
