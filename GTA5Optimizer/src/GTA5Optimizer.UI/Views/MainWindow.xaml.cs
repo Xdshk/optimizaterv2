9b0adba03fa8891cf464e1e6b7b0393ebfecbd32
juzik <sergejburov468@gmail.com>
2026-06-07 15:09:33 +0300

    chore: update GTA5Optimizer\src\GTA5Optimizer.UI\Views\MainWindow.xaml.cs

--- GTA5Optimizer/src/GTA5Optimizer.UI/Views/MainWindow.xaml.cs
+++ GTA5Optimizer/src/GTA5Optimizer.UI/Views/MainWindow.xaml.cs
@@ -1,18 +1,41 @@
+using GTA5Optimizer.UI.Services;
 using GTA5Optimizer.UI.ViewModels;
 
 namespace GTA5Optimizer.UI.Views;
 
 public partial class MainWindow : Window
 {
+    private readonly HotkeyService _hotkeyService;
+
     public MainWindow(MainWindowViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
+
+        _hotkeyService = new HotkeyService();
+        _hotkeyService.OptimizeRequested += async () =>
+        {
+            if (viewModel.OptimizeCommand.CanExecute(null))
+                await viewModel.OptimizeCommand.ExecuteAsync(null);
+        };
+
+        Loaded += (_, _) => _hotkeyService.Initialize(this);
+        Closed += (_, _) =>
+        {
+            _hotkeyService.Dispose();
+            viewModel.Dispose();
+        };
     }
 
     private void CloseButton_Click(object sender, RoutedEventArgs e)
     {
-        Close();
+        // Minimize to tray instead of closing
+        Hide();
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
     }
 
     private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
