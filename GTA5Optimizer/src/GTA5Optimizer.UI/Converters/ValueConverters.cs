8b8f32efcb896dd9d5e7b210e808d1dcf51650a5
juzik <sergejburov468@gmail.com>
2026-06-07 07:21:23 +0300

    chore: update GTA5Optimizer\src\GTA5Optimizer.UI\Converters\ValueConverters.cs

--- GTA5Optimizer/src/GTA5Optimizer.UI/Converters/ValueConverters.cs
+++ GTA5Optimizer/src/GTA5Optimizer.UI/Converters/ValueConverters.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace GTA5Optimizer.UI.Converters;
 
@@ -16,13 +17,19 @@ public class BytesToMBConverter : IValueConverter
         => throw new NotImplementedException();
 }
 
-public class GreaterThanConverter : IValueConverter
+public class SeverityToColorConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is double d && parameter is string s && double.TryParse(s, out var threshold))
-            return d > threshold;
-        return false;
+        if (value is double severity)
+        {
+            if (severity > 80)
+                return new SolidColorBrush(Color.FromRgb(0xFF, 0x44, 0x44));
+            if (severity > 50)
+                return new SolidColorBrush(Color.FromRgb(0xFF, 0xAA, 0x00));
+            return new SolidColorBrush(Color.FromRgb(0x00, 0xFF, 0x88));
+        }
+        return new SolidColorBrush(Color.FromRgb(0x88, 0x88, 0x88));
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
