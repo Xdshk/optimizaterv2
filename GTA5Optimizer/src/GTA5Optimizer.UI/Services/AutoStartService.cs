a18433a16070243a3ea22b45cf069fd76fa61be4
juzik <sergejburov468@gmail.com>
2026-06-07 15:06:43 +0300

    chore: add GTA5Optimizer\src\GTA5Optimizer.UI\Services\AutoStartService.cs

---
+++ GTA5Optimizer/src/GTA5Optimizer.UI/Services/AutoStartService.cs
@@ -0,0 +1,53 @@
+using Microsoft.Win32;
+
+namespace GTA5Optimizer.UI.Services;
+
+/// <summary>
+/// Управление автозапуском с Windows
+/// </summary>
+public static class AutoStartService
+{
+    private const string RegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+    private const string AppName = "GTA5Optimizer";
+
+    public static bool IsEnabled
+    {
+        get
+        {
+            try
+            {
+                using var key = Registry.CurrentUser.OpenSubKey(RegistryKey, false);
+                return key?.GetValue(AppName) != null;
+            }
+            catch { return false; }
+        }
+    }
+
+    public static void Enable()
+    {
+        try
+        {
+            var exePath = System.Reflection.Assembly.GetExecutingAssembly().Location
+                .Replace(".dll", ".exe");
+            using var key = Registry.CurrentUser.OpenSubKey(RegistryKey, true);
+            key?.SetValue(AppName, $"\"{exePath}\"");
+        }
+        catch { }
+    }
+
+    public static void Disable()
+    {
+        try
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(RegistryKey, true);
+            key?.DeleteValue(AppName, false);
+        }
+        catch { }
+    }
+
+    public static void Toggle()
+    {
+        if (IsEnabled) Disable();
+        else Enable();
+    }
+}
