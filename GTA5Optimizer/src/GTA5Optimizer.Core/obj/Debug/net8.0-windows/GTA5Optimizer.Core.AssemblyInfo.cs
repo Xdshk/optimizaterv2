defa133a2e303c71d22476056fa1ee0c9267b4cc
juzik <sergejburov468@gmail.com>
2026-06-12 15:56:47 +0300

    Initial commit: проект добавлен в репозиторий

--- GTA5Optimizer/src/GTA5Optimizer.Core/obj/Debug/net8.0-windows/GTA5Optimizer.Core.AssemblyInfo.cs
+++ GTA5Optimizer/src/GTA5Optimizer.Core/obj/Debug/net8.0-windows/GTA5Optimizer.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GTA5Optimizer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a219710ae0695df60e7949ac5dbef7cb1c9046")]
 [assembly: System.Reflection.AssemblyProductAttribute("GTA5Optimizer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GTA5Optimizer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
