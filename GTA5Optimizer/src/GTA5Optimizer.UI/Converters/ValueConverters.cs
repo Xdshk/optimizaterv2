d6ec144c5be882c4456b86dd91e835a77b87eb26
juzik <sergejburov468@gmail.com>
2026-06-14 02:42:28 +0300

    chore: update GTA5Optimizer\src\GTA5Optimizer.UI\Converters\ValueConverters.cs

--- GTA5Optimizer/src/GTA5Optimizer.UI/Converters/ValueConverters.cs
+++ GTA5Optimizer/src/GTA5Optimizer.UI/Converters/ValueConverters.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using MediaColor = System.Windows.Media.Color;
 using MediaSolidColorBrush = System.Windows.Media.SolidColorBrush;
